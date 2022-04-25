4fa3a533c8dc0bbee9a40a04577b46895f8fdf5d
RyanYJOh <hiimryan@kakao.com>
2022-04-25 23:02:28 +0900

    post api modified

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,8 @@ from . import views
 from rest_framework import routers
 
 urlpatterns = [
-    path('create/', views.postInvitation, name='post-invitation'),
-    path('create-service/', views.postService, name='post-service'),
-    path('clicks/', views.postClick, name='clicks'),
+    path('create', views.postInvitation, name='post-invitation'),
+    path('create-service', views.postService, name='post-service'),
+    path('clicks', views.postClick, name='clicks'),
     path('get-invitation/<int:service_id>', views.getRandomInvitation, name='get-random-invitation'),
 ]
